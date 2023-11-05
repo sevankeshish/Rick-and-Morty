328a1c55b613f122b28fae7644e126c8544312f9
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2023-11-05 15:03:11 +0400

    CharacterDetail component

--- src/components/CharecterDetail.jsx
+++ src/components/CharecterDetail.jsx
@@ -1,7 +1,57 @@
-import {} from "../../data/data";
+import { character, episodes } from "../../data/data";
+import { ArrowUpCircleIcon } from "@heroicons/react/24/outline";
 
 function CharecterDetail() {
-  return <div>CharecterDetail</div>;
+  return (
+    <div style={{ flex: 1 }}>
+      <div className="character-detail">
+        <img
+          src={character.image}
+          alt={character.name}
+          className="character-detail__img"
+        />
+        <div className="character-detail__info">
+          <h3 className="name">
+            <span>{character.gender === "Male" ? "👱‍♂️" : "👩"}</span>
+            <span>{character.name}</span>
+          </h3>
+          <div className="info">
+            <span
+              className={`status ${character.status === "Dead" ? "red" : ""}`}
+            ></span>
+            <span>&nbsp;{character.status}</span>
+            <span> - &nbsp;{character.species}</span>
+          </div>
+          <div className="location">
+            <p>Last known location :</p>
+            <p>{character.location.name}</p>
+          </div>
+          <div className="actions">
+            <button className="btn btn--primary">Add to favorite</button>
+          </div>
+        </div>
+      </div>
+      <div className="character-episodes">
+        <div className="title">
+          <h2>List of Episodes:</h2>
+          <button>
+            <ArrowUpCircleIcon className="icon" />
+          </button>
+        </div>
+        <ul>
+          {episodes.map((item, index) => (
+            <li key={item.id}>
+              <div>
+                {String(index + 1).padStart(2, "0")} - {item.episode} :{" "}
+                <strong>{item.name}</strong>
+              </div>
+              <div className="badge badge--secondary">{item.air_date}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default CharecterDetail;
